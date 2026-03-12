568f68be16afd100a4babbea5fcc9ac7002c4ab0
Dioni Zhong <dioniys@yahoo.com>
2026-03-12 13:58:12 +0800

    first draft

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'PaperAstro';
-export const SITE_DESCRIPTION = 'A hand‑drawn, lightweight Astro starter built with PaperCSS.';
-
+export const SITE_TITLE = "Dioni | meexia.com";
+export const SITE_DESCRIPTION = "Footprints of Dioni Zhong";
